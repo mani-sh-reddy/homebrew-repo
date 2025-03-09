fc40c2c8ac3957cf1600e4cb51d186bfa707b313
Mani Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2025-03-09 21:33:32 +0000

    Added fzf dependency on wrap

--- wrap.rb
+++ wrap.rb
@@ -1,9 +1,10 @@
 class Wrap < Formula
   desc "Bash utilities collection wrapped and accessible via keywords"
   homepage "https://github.com/mani-sh-reddy/wrap"
-  url "https://github.com/mani-sh-reddy/wrap", tag: "v0.0.2"
+  url "https://github.com/mani-sh-reddy/wrap/archive/v0.0.2.tar.gz"
   license "MIT"
-  head "https://github.com/mani-sh-reddy/wrap.git", branch: "main"
+  sha256 "9b8be7d62d80ae30a3dd38aab8e5a5fc7dca67042489728b260408b17d01c466"
+  version "0.0.2"
 
   def install
     bin.install "wrap.sh" => "wrap"
